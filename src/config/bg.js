d282ddbf1248d5f26ee12cb1e87de4b7db35570d
bin <316281400@qq.com>
2024-07-02 10:30:38 +0800

    更新

--- src/config/bg.js
+++ src/config/bg.js
@@ -14,9 +14,10 @@ export const bgImages = [
   { id: 'bg8', name: '背景8', src: getImgUrl('bg8.jpg') },
 ]
 
-export const bannerImages=[]
-
-for(let i = 0 ; i<5;i++) {
-  let name =  `${i}.jpeg`
-  bannerImages.push({id:`bg${i}`,name:`背景${i}`,src:getCDNPath(`/banner/banner${name}`)})
-}
+export const bannerImages=[
+  { id: 'banner0', name: 'banner0', src: getCDNPath(`/banner/banner0.jpeg`) },
+  { id: 'banner1', name: 'banner1', src: getCDNPath(`/banner/banner1.jpeg`) },
+  { id: 'banner2', name: 'banner2', src: getCDNPath(`/banner/banner2.jpeg`) },
+  { id: 'banner3', name: 'banner3', src: getCDNPath(`/banner/banner3.jpeg`) },
+  { id: 'banner4', name: 'banner4', src: getCDNPath(`/banner/banner4.jpeg`) },
+]
